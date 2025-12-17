3f53cedd245cfc257e0cd1540fe316ae8b750c71
Kay Nguyen <tungntt3108@gmail.com>
2025-12-17 15:43:32 +0700

    initialize the project

---
+++ src/main/java/com/xphr/reporting/service/Application.java
@@ -0,0 +1,13 @@
+package com.xphr.reporting.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentReportingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentReportingServiceApplication.class, args);
+    }
+
+}
