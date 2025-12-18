f41960423fd3199e130f07b255f055cddecf5ac9
Kay Nguyen <tungntt3108@gmail.com>
2025-12-18 19:44:58 +0700

    remove used variable

--- src/main/java/com/xphr/reporting/ms/controller/dto/PagingListResponseDto.java
+++ src/main/java/com/xphr/reporting/ms/controller/dto/PagingListResponseDto.java
@@ -1,6 +1,5 @@
 package com.xphr.reporting.ms.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,18 +13,13 @@ import java.util.List;
 @AllArgsConstructor
 public class PagingListResponseDto<T> {
 
-    @JsonProperty("current_page")
     private Integer currentPage;
 
-    @JsonProperty("page_size")
     private Integer pageSize;
 
-    @JsonProperty("total_pages")
     private Integer totalPage;
 
-    @JsonProperty("total_records")
     private Long totalRecord;
 
-    @JsonProperty("data")
     private List<T> data;
 }
