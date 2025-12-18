e1709acc126fb040f96f2f24324cb815877793f6
Kay Nguyen <tungntt3108@gmail.com>
2025-12-18 19:48:16 +0700

    remove used variable

--- src/main/java/com/xphr/reporting/ms/controller/dto/TimeRecordReportDto.java
+++ src/main/java/com/xphr/reporting/ms/controller/dto/TimeRecordReportDto.java
@@ -1,23 +1,15 @@
 package com.xphr.reporting.ms.controller.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class TimeRecordReportDto {
 
     private String employeeName;
     private String projectName;
     private Double totalHours;
-
-    public TimeRecordReportDto(String employeeName, String projectName, Double totalHours) {
-        this.employeeName = employeeName;
-        this.projectName = projectName;
-        this.totalHours = totalHours;
-    }
 
     public String getFormattedHours() {
         if (totalHours == null) return "0.00";
